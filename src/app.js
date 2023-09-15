1dbb7243e5f0305e9ee1fa5eb2f7b46e8b3c5438
codewithrabeeh <rabeehebrahim48@gmail.com>
2023-09-15 17:26:39 +0530

    feat: cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const helmet = require('helmet')
 const rateLimiter = require('./middlewares/rateLimiter')
-const logger = require('./utils/winstonLogger')
+const cors = require('cors')
 
 const app = express()
 
 app.use(rateLimiter)
+app.use(cors())
 app.use(helmet())
 app.use(express.json({limit: '5mb'}))
 app.use(express.urlencoded({extended: true, limit: '5mb'}))
