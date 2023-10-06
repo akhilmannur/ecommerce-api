ead5a77187bbde26d4d01cb468e3caeaa0607284
Rabeeh Ebrahim <rabeehebrahim48@gmail.com>
2023-10-06 11:06:38 +0530

    fix: ratelimit to 200 req per 15 minutes

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit')
 
 const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 Minutes in milliseconds
-  max: 100,
-  message: 'You have exceeded the 100 requests in 15 Minutes limit!', 
+  max: 200,
+  message: 'You have exceeded the number of API calls in 15 Minutes limit!', 
   standardHeaders: true,
   legacyHeaders: false,
 })
