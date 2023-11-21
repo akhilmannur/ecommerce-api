139693b5dc3b82e962fc498d5f3e9ad85a8a7e9f
codewithrabeeh <rabeehebrahim48@gmail.com>
2023-11-21 19:48:11 +0530

    fix: keygenerator in rateLimiter

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,11 +1,12 @@
-const rateLimit = require('express-rate-limit')
+const rateLimit = require("express-rate-limit");
 
 const rateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 Minutes in milliseconds
+  windowMs: 15 * 60 * 1000, // 15 Minutes in milliseconds
   max: 200,
-  message: 'You have exceeded the number of API calls in 15 Minutes limit!', 
+  message: "You have exceeded the number of API calls in 15 Minutes limit!",
   standardHeaders: true,
   legacyHeaders: false,
-})
+  keyGenerator: (req, res) => process.env.NODE_ENV === 'development' ? req.ip : req.headers["x-real-ip"]
+});
 
-module.exports = rateLimiter
+module.exports = rateLimiter;
